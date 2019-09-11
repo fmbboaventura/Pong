783fcdc0e42fa03c4f01dff6e3c45d3e263afd9e
Filipe Boaventura <fmbboaventura@gmail.com>
2019-09-11 00:17:17 -0300

    Detectando quando a bola sai da tela

--- scripts/Stage/BallControl.gd
+++ scripts/Stage/BallControl.gd
@@ -6,10 +6,19 @@ export (int) var speed = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	velocity.x -= 1 if (randi() % 2) else -1
-	velocity.y -= 1 if (randi() % 2) else -1
+	reset_ball()
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity.normalized() * speed * delta)
 	if collision:
         velocity = velocity.bounce(collision.normal)
+	elif not get_node("VisibilityNotifier2D").is_on_screen():
+		reset_ball()
+
+func reset_ball():
+	position.x = 0
+	position.y = 0
+	
+	velocity = Vector2()
+	velocity.x -= 1 if (randi() % 2) else -1
+	velocity.y -= 1 if (randi() % 2) else -1
