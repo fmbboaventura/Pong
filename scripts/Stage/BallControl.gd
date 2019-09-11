00c2b3f58f844eedfe23594d821215ea0b4beb64
Filipe Boaventura <fmbboaventura@gmail.com>
2019-09-11 00:33:12 -0300

    Declarando sinais quando a bola deixa a tela pela esquerda ou direita

--- scripts/Stage/BallControl.gd
+++ scripts/Stage/BallControl.gd
@@ -4,15 +4,22 @@ var velocity = Vector2()
 
 export (int) var speed = 200
 
+signal exit_screen_left
+signal exit_screen_right
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	reset_ball()
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity.normalized() * speed * delta)
+	
 	if collision:
         velocity = velocity.bounce(collision.normal)
+		
 	elif not get_node("VisibilityNotifier2D").is_on_screen():
+		if position.x < 0: emit_signal("exit_screen_left")
+		else: emit_signal("exit_screen_right")
 		reset_ball()
 
 func reset_ball():
